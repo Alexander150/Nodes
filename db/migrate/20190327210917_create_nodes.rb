c8b798adc651296241edd97c5f9039bd4c1fb0a1
zarnik119 <zarnik119@gmail.com>
2019-03-28 00:10:31 +0300

    Создана модель Node, создана админ панель

---
+++ db/migrate/20190327210917_create_nodes.rb
@@ -0,0 +1,11 @@
+class CreateNodes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :nodes do |t|
+      t.string :name
+      t.text :body
+      t.references :nodes, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
